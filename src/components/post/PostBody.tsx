0ece65da36613989c861e287b88a2248a87a016b
위영민 <youngminieo1005@gmail.com>
2024-07-29 10:03:52 +0900

    minor

--- src/components/post/PostBody.tsx
+++ src/components/post/PostBody.tsx
@@ -1,31 +1,13 @@
-import { rgbDataURL } from "@/functions/image";
 import { TPost } from "@/types/post";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import Image from "next/image";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
 
 const PostBody = ({ post }: { post: TPost }) => {
-  const { thumbnail } = post;
-
   return (
     <div className="flex flex-1 flex-col">
-      {thumbnail && (
-        <div className="relative aspect-[1.618]">
-          <Image
-            className="mx-auto"
-            src={thumbnail}
-            alt="post-body-thumbnail"
-            layout="fill"
-            objectFit="cover"
-            placeholder="blur"
-            blurDataURL={rgbDataURL(233, 233, 233)}
-          />
-        </div>
-      )}
-
       <MDXRemote
         source={post.content}
         options={{
@@ -35,7 +17,7 @@ const PostBody = ({ post }: { post: TPost }) => {
               [
                 rehypePrettyCode,
                 {
-                  theme: { dark: "github-dark-dimmed", light: "github-light" },
+                  theme: { dark: "github-dark", light: "github-light" },
                 },
               ],
               rehypeSlug,
--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -1,14 +1,48 @@
+import { rgbDataURL } from "@/functions/image";
 import { TPost } from "@/types/post";
+import Image from "next/image";
 
 const PostHeader = ({ post }: { post: TPost }) => {
-  const { title, subTitle, readingMinutes, createdAt } = post;
+  const { title, introduction, readingMinutes, thumbnail, createdAt } = post;
+
+  const createdDateOnly = createdAt.split(" ")[0];
 
   return (
     <div className="flex flex-col">
-      <h2>{title}</h2>
-      <h3>{subTitle}</h3>
-      <span>{readingMinutes}</span>
-      <span>{createdAt}</span>
+      <h2 className="font-pretendard text-[3.2rem] font-bold">{title}</h2>
+
+      <div className="flex flex-col items-end">
+        <span>
+          Reading Time :{" "}
+          <span className="font-pretendard text-[1.1rem] font-semibold">
+            {readingMinutes} min
+          </span>
+        </span>
+        <span>
+          Created Date :{" "}
+          <span className="font-pretendard text-[1.1rem] font-semibold">
+            {createdDateOnly}
+          </span>
+        </span>
+      </div>
+
+      {thumbnail && (
+        <div className="relative my-[1.6rem] aspect-[1.618]">
+          <Image
+            className="mx-auto"
+            src={thumbnail}
+            alt="post-body-thumbnail"
+            layout="fill"
+            objectFit="cover"
+            placeholder="blur"
+            blurDataURL={rgbDataURL(233, 233, 233)}
+          />
+        </div>
+      )}
+
+      <p className="font-pretendard text-[1.6rem] font-normal">
+        {introduction}
+      </p>
     </div>
   );
 };

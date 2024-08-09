945c9ff23d63f8b22120cb78102d1b8ff9478632
위영민 <youngminieo1005@gmail.com>
2024-08-09 18:11:25 +0900

    [style]: Some cute

--- src/components/post/PostToC.tsx
+++ src/components/post/PostToC.tsx
@@ -3,13 +3,17 @@
 import { useRouter } from "next/navigation";
 import { MouseEventHandler, useEffect, useState } from "react";
 
+// 1. 렌딩되었을때(스크롤 X)도 보이도록
+// 2. 현재 보고 있는 섹션 하이라이팅
+
 interface IToCItem {
   level: string;
   text: string | null;
   href: string;
+  absoluteTop: number;
 }
 
-const levelToStyleClassNameMap: { [key: string]: string } = {
+const levelIndentClassNameMap: { [key: string]: string } = {
   H1: "p-0",
   H2: "p-2",
   H3: "p-4",
@@ -19,10 +23,13 @@ const PostToC = () => {
   const isServerSide = typeof window === "undefined";
 
   const router = useRouter();
+  const [scrollY, setScrollY] = useState(isServerSide ? 0 : window.scrollY);
   const [isSticky, setIsSticky] = useState<boolean>();
 
   const [postToCList, setPostToCList] = useState<IToCItem[]>([]);
   const isToCReady = postToCList.length !== 0;
+
+  const [highlightToCItem, setHighlightToCItem] = useState<IToCItem>();
 
   const handleToCItemClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
     event.preventDefault();
@@ -44,36 +51,40 @@ const PostToC = () => {
   };
 
   useEffect(() => {
-    if (!isServerSide) {
-      const $postBody = document.getElementById("post-body");
+    const $postBody = document.getElementById("post-body");
 
-      if ($postBody) {
-        // step 1 : h1 ~ h3 태그까지 추출
-        const hTagList = $postBody.querySelectorAll("h1, h2, h3"); // h1, h2, h3 정도만 ToC 로 활용
+    if ($postBody) {
+      // step 1 : h1 ~ h3 태그까지 추출
+      const hTagList = $postBody.querySelectorAll("h1, h2, h3"); // h1, h2, h3 정도만 ToC 로 활용
 
-        // step 2 : ToC 리스트 만들기 (item = tagName, text, id)
-        const postToCListTemp: IToCItem[] = [];
+      // step 2 : ToC 리스트 만들기 (item = tagName, text, id)
+      const postToCListTemp: IToCItem[] = [];
 
-        hTagList.forEach((hTagItem) => {
-          const { id: href, nodeName: level, textContent: text } = hTagItem;
-          postToCListTemp.push({ level, text, href });
+      hTagList.forEach((hTagItem) => {
+        const { id: href, nodeName: level, textContent: text } = hTagItem;
+
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+        const absoluteTop = hTagItem.getBoundingClientRect().top + scrollTop;
+
+        postToCListTemp.push({
+          level,
+          text,
+          href,
+          absoluteTop: absoluteTop,
         });
+      });
 
-        if (postToCListTemp.length !== 0) {
-          setPostToCList(postToCListTemp);
-        }
+      if (postToCListTemp.length !== 0) {
+        setPostToCList(postToCListTemp);
       }
     }
   }, []);
 
   useEffect(() => {
     const handleScrollForSticky = () => {
-      if (window.scrollY >= 300) {
-        setIsSticky(true);
-        return;
-      }
-
-      setIsSticky(false);
+      const newIsSticky = window.scrollY >= 300 ? true : false;
+      setIsSticky(newIsSticky);
+      setScrollY(window.scrollY);
     };
 
     if (!isServerSide) {
@@ -85,23 +96,55 @@ const PostToC = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const handleScrollForHighlight = () => {
+      if (postToCList.length !== 0) {
+        const candidateHighlightToCList = postToCList.filter((postToCItem) => {
+          const { absoluteTop } = postToCItem;
+
+          return (
+            scrollY <= absoluteTop &&
+            absoluteTop < scrollY + document.documentElement.clientHeight
+          );
+        });
+
+        setHighlightToCItem(candidateHighlightToCList?.at(0));
+      }
+    };
+
+    window.addEventListener("scroll", handleScrollForHighlight);
+
+    return () => {
+      window.addEventListener("scroll", handleScrollForHighlight);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollY]);
+
   return (
     <aside
-      className={`${isToCReady ? `opacity-100 transition-opacity duration-300` : `opacity-0`} ${isSticky ? `fixed top-[12rem]` : `absolute top-[30rem] mt-[12rem]`} right-[calc(50%_-_32rem_-_20rem_-_3rem)] !w-[20rem] rounded-[0.8rem] border-[0.2rem] border-solid border-[var(--foreground)] p-[1.2rem] max-pc:hidden`}
+      className={`${isToCReady ? `opacity-100 transition-opacity duration-300` : `opacity-0`} ${isSticky ? `fixed top-[12rem]` : `absolute top-[30rem] mt-[12rem]`} right-[calc(50%_-_32rem_-_20rem_-_3rem)] max-pc:hidden`}
     >
-      {postToCList.map((postToCItem, index) => {
-        const { level, text, href } = postToCItem;
-        return (
-          <div
-            key={index}
-            className={`${levelToStyleClassNameMap[level]} py-[0.4rem] text-[var(--foreground)] hover:text-cyan-600`}
-          >
-            <a href={`#${href}`} onClick={handleToCItemClick}>
-              {text}
-            </a>
-          </div>
-        );
-      })}
+      <div className="relative !w-[20rem] rounded-[0.8rem] border-[0.2rem] border-solid border-[var(--foreground)] px-[1.2rem] py-[0.8rem]">
+        <div className="absolute right-0 top-0 flex -translate-y-1/2 translate-x-1/2 items-center justify-center bg-[var(--background)] pl-[0.4rem] text-[3rem]">
+          📌
+        </div>
+        {postToCList.map((postToCItem, index) => {
+          const { level, text, href } = postToCItem;
+
+          const isHighlight = href === highlightToCItem?.href;
+
+          return (
+            <div
+              key={index}
+              className={`${levelIndentClassNameMap[level]} ${isHighlight ? `font-semibold text-[var(--highlight)]` : ``} text-[var(--foreground) py-[0.4rem] text-[1.2rem]`}
+            >
+              <a href={`#${href}`} onClick={handleToCItemClick}>
+                {text}
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </aside>
   );
 };

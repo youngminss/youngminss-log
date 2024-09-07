8453436bf74abf423819c07f168975f9db36480e
위영민 <youngminieo1005@gmail.com>
2024-09-07 18:15:23 +0900

    [style]: Move some component location

--- src/components/layout/FloatingActions.tsx
+++ src/components/layout/FloatingActions.tsx
@@ -3,25 +3,25 @@
 import { Grip } from "lucide-react";
 import { useState } from "react";
 import ScrollToTopButton from "../buttons/ScrollToTopButton";
-import ThemeMode from "../buttons/ThemeModeButton";
+import ThemeModeButton from "../buttons/ThemeModeButton";
+
 const FloatingActions = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="fixed bottom-[2.8rem] right-[6.8rem] transition-all">
+    <div className="fixed bottom-[2rem] right-[2rem]">
+      <ScrollToTopButton
+        className={`absolute bottom-0 right-[4.8rem] z-[3] cursor-pointer border-[0.15rem] border-solid border-[var(--foreground)] bg-[var(--background)] transition-[transform] duration-100 active:scale-[0.9]`}
+      />
+
+      <ThemeModeButton
+        type="toggle"
+        className={`absolute bottom-0 right-0 z-[2] rounded-[0.4rem] border-[0.15rem] border-solid border-[--foreground] bg-[var(--background)] p-[0.8rem] transition-[transform] duration-100 will-change-transform ${open ? `-translate-y-[4.8rem]` : `translate-y-0`} cursor-pointer active:scale-[0.9]`}
+      />
+
       <Grip
         size={40}
-        className="absolute bottom-0 z-[3] cursor-pointer rounded-[0.4rem] border-[0.15rem] border-solid border-[--foreground] bg-[var(--background)] p-[0.8rem] hover:bg-[var(--foreground)] hover:text-[var(--background)]"
-        onClick={() => setOpen(!open)}
-      />
-
-      <ScrollToTopButton
-        className={`absolute z-[2] cursor-pointer border-[0.15rem] border-solid border-[var(--foreground)] duration-200 hover:bg-[var(--foreground)] hover:text-[var(--background)] ${open ? "bottom-[4.8rem] bg-[var(--background)] pc:bg-transparent" : "bottom-0"}`}
-        onClick={() => setOpen(!open)}
-      />
-
-      <ThemeMode
-        className={`absolute z-[1] cursor-pointer border-[0.15rem] border-solid border-[var(--foreground)] duration-200 hover:bg-[var(--foreground)] hover:text-[var(--background)] ${open ? "bottom-[9.6rem] bg-[var(--background)] pc:bg-transparent" : "bottom-0"}`}
+        className="absolute bottom-0 right-0 z-[3] cursor-pointer rounded-[0.4rem] border-[0.15rem] border-solid border-[--foreground] bg-[var(--background)] p-[0.8rem]"
         onClick={() => setOpen(!open)}
       />
     </div>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,22 @@
+import { AUTHOR_NAME, GITHUB_PROFILE } from "@/utils/const";
+import { Github } from "lucide-react";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="flex h-[5.6rem] w-full items-center justify-center">
-      <p className="font-pretendard text-[1.4rem] font-bold">
-        Copyright 2024. Young-min all rights reserved
-      </p>
+    <footer className="w-full py-[2.8rem]">
+      <div className="flex flex-col items-center gap-y-[0.8rem]">
+        <Link href={GITHUB_PROFILE} target="_blank">
+          <Github
+            size={40}
+            className="rounded-[0.4rem] bg-opacity-80 p-[0.8rem] transition-[transform] duration-100 will-change-transform active:scale-[0.9]"
+          />
+        </Link>
+
+        <p className="font-pretendard text-[1.4rem] font-bold">
+          Copyright 2024. {AUTHOR_NAME} all rights reserved
+        </p>
+      </div>
     </footer>
   );
 };

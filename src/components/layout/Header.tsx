d1132c071d89e7477b5aff66561c53450199747d
위영민 <youngminieo1005@gmail.com>
2024-09-18 16:06:06 +0900

    minor

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,3 @@
-import { Book } from "lucide-react";
 import Link from "next/link";
 import ScrollProgress from "./ScrollProgress";
 
@@ -12,19 +11,6 @@ const Header = () => {
         <h1 className="font-pretendard text-[3.2rem] font-black">
           <Link href="/">영민하다</Link>
         </h1>
-
-        <nav className="ml-auto font-pretendard font-medium">
-          <ul className="flex items-center gap-x-[1.2rem]">
-            <li>
-              <Link href="/posts">
-                <Book
-                  size={40}
-                  className="rounded-[0.4rem] bg-opacity-80 p-[0.4rem] transition-[transform] duration-100 will-change-transform active:scale-[0.8]"
-                />
-              </Link>
-            </li>
-          </ul>
-        </nav>
 
         <ScrollProgress />
       </div>

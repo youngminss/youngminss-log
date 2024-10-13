87ead55c83afb12f6e827e22f5c470fa1ddf6c7a
위영민 <youngminieo1005@gmail.com>
2024-10-13 15:57:37 +0900

    [feat]: Added GTM by @next/third-parties/google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import ThemeProvider from "@/components/providers/ThemeProvider";
 import Sonnar from "@/components/ui/Sonnar";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -70,6 +70,7 @@ export default function RootLayout({
           />
         </ThemeProvider>
 
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID} />
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
       </body>
     </html>

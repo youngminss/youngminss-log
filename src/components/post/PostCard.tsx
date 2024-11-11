07f8bb9cd75c96cc2330f40c4ee7751132a3d993
위영민 <youngminieo1005@gmail.com>
2024-11-11 11:52:56 +0900

    [style]: Remove PostCard shadow color

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -9,7 +9,7 @@ const PostCard = async ({ post }: { post: TPost }) => {
 
   return (
     <Link href={url} className="group">
-      <div className="relative flex justify-between gap-x-[1.6rem] overflow-clip transition-all duration-200 max-postCard:mx-[1.6rem] max-postCard:flex-col-reverse max-postCard:gap-y-[1.6rem] max-postCard:rounded-[0.4rem] max-postCard:shadow-lg max-postCard:shadow-[var(--foreground)] max-postCard:group-active:scale-95 postCard:px-[1.6rem] postCard:py-[2.4rem] postCard:will-change-transform postCard:group-hover:translate-x-[0.8rem] postCard:group-hover:bg-[var(--foreground)] postCard:group-hover:text-[var(--background)]">
+      <div className="relative flex justify-between gap-x-[1.6rem] overflow-clip transition-all duration-200 max-postCard:mx-[1.6rem] max-postCard:flex-col-reverse max-postCard:gap-y-[1.6rem] max-postCard:rounded-[0.4rem] max-postCard:border max-postCard:border-solid max-postCard:border-[var(--foreground)] max-postCard:shadow-lg max-postCard:group-active:scale-95 postCard:px-[1.6rem] postCard:py-[2.4rem] postCard:will-change-transform postCard:group-hover:translate-x-[0.8rem] postCard:group-hover:bg-[var(--foreground)] postCard:group-hover:text-[var(--background)]">
         <div className="flex flex-1 flex-col justify-between max-postCard:px-[1.6rem] max-postCard:pb-[1.6rem]">
           <div className="flex flex-col gap-y-[1.2rem]">
             <h2 className="text-[2.2rem] font-bold">{title}</h2>

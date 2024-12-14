5a0f947079cd1c2c251e3ba8b17a1468e259b943
위영민 <youngminieo1005@gmail.com>
2024-12-14 17:48:20 +0900

    [style]: Contrast background and foreground for text, Add aria-label for icon link button

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,11 @@ const Footer = () => {
   return (
     <footer className="w-full py-[2.8rem]">
       <div className="flex flex-col items-center gap-y-[0.8rem]">
-        <Link href={GITHUB_PROFILE} target="_blank">
+        <Link
+          href={GITHUB_PROFILE}
+          target="_blank"
+          aria-label="Go to github profile on Footer"
+        >
           <Github
             size={40}
             className="rounded-[0.4rem] bg-opacity-80 p-[0.8rem] transition-[transform] duration-100 will-change-transform active:scale-[0.9]"

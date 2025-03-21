f834b31ec3a886af1b95b5fc10573603fa8101eb
위영민 <youngminieo1005@gmail.com>
2025-03-21 14:55:01 +0900

    chore: update tailwindcss version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   prefix: "",
   theme: {
     container: {
